0c1ff480a4ec60e8db674aa67a98b829dce136a8
Christian Mahnke <cmahnke@gmail.com>
2021-05-16 02:40:27 +0200

    Updated cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 ./themes/projektemacher-base/scripts/cleanup.sh
+rm static/images/logo.png
 find content/post/ -name preview.png | xargs rm
+find content/post/ -name ogPreview.svg | xargs rm
+find content/post/ -name ogPreview.png | xargs rm
