ca9279c009c04b49e573f61b392bec9f9c1ca0b0
Christian Mahnke <cmahnke@gmail.com>
2023-06-05 19:59:20 +0200

    Fix build error

---
+++ scripts/favicon.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Favicons
+SOURCE=Source\ Files/Favicon/Favicon.psd[1] OPTIONS="-background 'rgba(255, 255, 255, .0)' -resize 300x300 -gravity center -extent 300x300" ./themes/projektemacher-base/scripts/favicon.sh
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -25,5 +25,4 @@ convert "Source Files/Favicon/Favicon.psd[1]" static/images/logo.png
 # Generate Previews
 ./themes/projektemacher-base/scripts/preview.sh
 
-# Favicons
-SOURCE="Source Files/Favicon/Favicon.psd[1]" OPTIONS="-background 'rgba(255, 255, 255, .0)' -resize 300x300 -gravity center -extent 300x300" ./themes/projektemacher-base/scripts/favicon.sh
+./scripts/favicon.sh
