6ae004ae554b6e89b04c201836660f27810ef00f
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2025-10-04 11:27:45 +0200

    Try to fix build

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -7,4 +7,3 @@ $CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-inkscape.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
-$CTX_PATH/../themes/projektemacher-base/scripts/github/setup-yarn.sh
